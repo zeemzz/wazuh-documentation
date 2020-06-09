2d859b0f2b60842f6845755a2356c8926c7e0e02
vamera <vanesa.mejias@wazuh.com>
2020-06-09 17:43:07 +0200

    Fixed the behavior of the scroll to an endpoint in API reference page

--- source/_static/js/custom-swagger-ui.js
+++ source/_static/js/custom-swagger-ui.js
@@ -3,13 +3,13 @@ jQuery(function($) {
   // id   => operations-agents-api\.controllers\.agents_controller\.get_agent_upgrade
   const hash = window.location.href.split('#')[1] || '';
 
-  hashAction(hash);
+  init(hash);
 
   /**
-    * Starts action depending on hash
+    * Starts the functionality only when the main content is loaded
     * @param {string} hash String that appearse after the sign # in the URL.
     */
-  function hashAction(hash) {
+  function init(hash) {
     let swaggerObserver = null;
     const config = {childList: true, subtree: true};
     const params = hashToId(hash);
@@ -21,10 +21,18 @@ jQuery(function($) {
         && !$(mutationsList[i].addedNodes[0]).hasClass('information-container')) {
           // When Swagger is finally loaded
           if ($(mutationsList[i].addedNodes[0]).find('.opblock-tag-section').length > 0) {
+            // Create and observer for every tag section
             $('.opblock-tag').each(function() {
-              tags.push($(this).data('tag'));
+              const currentTag = $(this).data('tag');
+              tags.push(currentTag);
+              // $(this).on('click', function(){
+              // window.location.hash = currentTag;
+              // });
+              observeTagSection(params, currentTag, config );
             });
-            openOperation(params, tags, config);
+
+            // openOperation(params, tags, config);
+            swaggerObserver.disconnect();
           }
         }
       }
@@ -36,27 +44,30 @@ jQuery(function($) {
   }
 
   /**
-    * Opens the operation tag specified in the hash
+    * Observes and react to the changes in a particular tag section
     * @param {obj} params Info extracted from the hash. Contains target id, operation, and endpoint identification
-    * @param {array} tags List of operation tags that were loaded
-    * @param {obj} config configuration for the observer
+    * @param {string} tag Name of the tag being checked
+    * @param {obj} config Configuration for the observer
     */
-  function openOperation(params, tags, config) {
+  function observeTagSection(params, tag, config) {
     let operationObserver = null;
+    // console.log($('#operations-tag-'+tag).parent()[0]);
     const operationLoaded = function(mutationsList, observer) {
       for (i = 0; i < mutationsList.length; i++) {
-        if (mutationsList[i].type === 'childList') {
+        if ( mutationsList[i].type === 'childList' && $(mutationsList[i].addedNodes[0]).find('.opblock').length > 0 ) {
+          // When operation block is finally loaded:
           $(mutationsList[i].addedNodes[0]).find('.opblock').each(function() {
             $(this).attr('id', $(this).attr('id').replace(/\\\./g, '-dot-'));
           });
-          // When operation block is finally loaded
           scrollToEndpoint(params.id);
         }
       }
     };
     operationObserver = new MutationObserver(operationLoaded);
-    operationObserver.observe($('#operations-tag-'+params.operation).parent()[0], config);
-    $('#operations-tag-'+params.operation).click();
+    operationObserver.observe($('#operations-tag-'+tag).parent()[0], config);
+    if ( tag === params.operation ) {
+      $('#operations-tag-'+params.operation).click();
+    }
   }
 
   /**
@@ -65,22 +76,26 @@ jQuery(function($) {
     */
   function scrollToEndpoint(id) {
     if ( $('#'+id).length > 0 ) {
-      $('html, body').animate({
-        scrollTop: $('#'+id).offset().top - $('#header-sticky').height()-10,
-      }, 200);
+      $('html, body').scrollTop($('#'+id).offset().top - $('#header-sticky').height()-10);
     }
   }
 
   /**
-    * Extract the inforation from the hash
+    * Extract the information from the hash
     * @param {string} hash String that contains the information to scroll to an element
     * @return {object} Object containing the sturecured information extracted from the hash
     */
   function hashToId(hash) {
     const result = {};
     result.hash = hash;
-    [result.operation, result.endpoint] = hash.split('+');
-    result.id = 'operations-' + result.operation + '-api-dot-controllers-dot-' + result.operation + '_controller-dot-' + result.endpoint;
+    if ( hash.length > 0) {
+      [result.operation, result.endpoint] = hash.split('+');
+      if ( result.endpoint != undefined ) {
+        result.id = 'operations-' + result.operation + '-api-dot-controllers-dot-' + result.operation + '_controller-dot-' + result.endpoint;
+      } else {
+        result.id = hash;
+      }
+    }
     return result;
   }
 });
